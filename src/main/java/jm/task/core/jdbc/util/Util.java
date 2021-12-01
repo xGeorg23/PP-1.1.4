7c077e374687cec3099f4a20ef86ded09d34b746
xGeorg23 <89738550+xGeorg23@users.noreply.github.com>
2021-12-01 14:57:06 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,48 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.*;
+import java.util.Properties;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String url = "jdbc:mysql://localhost:3306/mytest";
+    public static final String username = "root";
+    public static final String password = "root1";
+    
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("У нас проблемы");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    // Hibernate
+    private static SessionFactory sessionFactory = null;
+
+    static {       // статический блок, что бы инициализировать настройки хибернейт один раз
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("hibernate.connection.url", url);
+            properties.setProperty("hibernate.connection.username", username);
+            properties.setProperty("hibernate.connection.password", password);
+            properties.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+
+            sessionFactory = new Configuration().addAnnotatedClass(User.class).addProperties(properties).buildSessionFactory();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
+    }
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
 }
